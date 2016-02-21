bffb2edb4db7e2a245c46717c0c919e269916ff5
chethtra yen <chethtra_yen@rocketmail.com>
2016-02-21 10:50:58 -0500

    or so high level

--- witPy/conch.py
+++ witPy/conch.py
@@ -4,49 +4,59 @@ import wit
 import json
 import os
 wit.init()
-while(1):
+run =1
+while(run):
 # obtain audio from the microphone
-	r = sr.Recognizer()
-	with sr.Microphone() as source:
-	    print("Say something!")
-	    audio = r.listen(source)
+	try:
+		r = sr.Recognizer()
+		with sr.Microphone() as source:
+		    print("Say something!")
+		    audio = r.listen(source)
 
 
-	 #try:
-	    # for testing purposes, we're just using the default API key
-	    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-	    # instead of `r.recognize_google(audio)`
-	    #print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-	inputSpeech = r.recognize_google(audio)
-	print(inputSpeech)
+		 #try:
+		    # for testing purposes, we're just using the default API key
+		    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+		    # instead of `r.recognize_google(audio)`
+		    #print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+		inputSpeech = r.recognize_google(audio)
+		print(inputSpeech)
 
 
-	#print(format(wit.text_query(inputSpeech, "OK3GYG6TYL4YGW7RTSXNCA4AFSK4Y2JD")))
-	parsedCommand = json.loads(format(wit.text_query(inputSpeech, "OK3GYG6TYL4YGW7RTSXNCA4AFSK4Y2JD")))
+		#print(format(wit.text_query(inputSpeech, "OK3GYG6TYL4YGW7RTSXNCA4AFSK4Y2JD")))
+		parsedCommand = json.loads(format(wit.text_query(inputSpeech, "OK3GYG6TYL4YGW7RTSXNCA4AFSK4Y2JD")))
 
 
-	intent = parsedCommand['outcomes'][0]['intent']
+		intent = parsedCommand['outcomes'][0]['intent']
 
 
-	if intent == "list":
-		print("list")
-		files = [f for f in os.listdir('.') if os.path.isfile(f)]
-		i = 1
-		call(["say", "Here is the list of file"])
-		for f in files:
-			call(["say", str(i)])
-			call(["say", f])
-			i = i+1
-	elif intent == "echo":
-		print("--- ECHO COMMAND ---")
-		value = parsedCommand['outcomes'][0]['entities']['echo_text'][0]['value']
-		call(["say", value])
-	elif intent == "change_directory":
-		call(["say","I change dat directory"])
-		#os.chdir('../')
-	else:
-		call(["say", "Sorry, I didn't catch that."])
-
+		if intent == "list":
+			print("list")
+			files = [f for f in os.listdir('.') if os.path.isfile(f)]
+			i = 1
+			call(["say", "Here is the list of file"])
+			for f in files:
+				call(["say", str(i)])
+				call(["say", f])
+				i = i+1
+		elif intent == "echo":
+			print("--- ECHO COMMAND ---")
+			value = parsedCommand['outcomes'][0]['entities']['echo_text'][0]['value']
+			call(["say", value])
+		elif intent == "change_directory":
+			
+			try:
+				call(["say","I change dat directory"])
+				value = parsedCommand['outcomes'][0]['entities']['echo_text'][0]['value']
+				os.chdir(value)
+			except:
+				call(["say","I failed you"])
+		elif inputSpeech == "quit" or inputSpeech == "exit":
+			run = 0
+		else:
+			call(["say", "Sorry, I didn't catch that."])
+	except:
+		run = 1		
 
 	#print json.dumps(parsedCommand, sort_keys=True, indent=4)
 wit.close()
